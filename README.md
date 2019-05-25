32159c52e845a60be2596881e4e6715b1b70c177
Tyler G <tyler.getson@gmail.com>
2019-05-25 17:20:17 -0400

    intial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nexx Garage Node for NodeRED
+
+A simple node that opens a nexx garage powered garage.
+
+## Installation
+
+Follow the instructions [here](https://github.com/tget4/nexx-garage-sdk) to find Client ID and Device Token
---
+++ nexx-garage.js
@@ -0,0 +1,36 @@
+const { NexxApiClient } = require('nexx-garage-sdk');
+
+let client;
+module.exports = function(RED) {
+  function node (config) {
+      
+    RED.nodes.createNode(this, config);
+    const node = this;
+    node.send({ payload: config });
+    client = new NexxApiClient(config);
+
+    if (process.env.NODE_ENV === 'development') {
+      client.logger = (payload) => {
+          node.send({  payload });
+      };
+    }
+
+    this.on('input', async (msg) => {
+      try {
+        await client.generateToken();
+        const devices = await client.getDevices();
+        await client.open(devices[0].DeviceId);
+        node.send(msg);
+      } catch (e) {
+        node.send({ payload: e});
+      }
+    });
+  };
+
+  RED.nodes.registerType("NexxGarage", node);
+  RED.httpAdmin.get(`/NexxGarage/devices`, (req, res) => {
+    client.getDevices().then((devices) => {
+      res.send(devices);
+    });
+  });
+}
